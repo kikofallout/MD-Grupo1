5d7aed58ed269bde1e2c840a44e93562b41baf64
mayagomes039 <a100822@alunos.uminho.pt>
2025-04-11 15:26:38 +0100

    dados em JSON para roleE

---
+++ Data/src/DATA_EXPORT/export_data.py
@@ -0,0 +1,14 @@
+import pymongo
+from pymongo import MongoClient
+import json
+
+# Conectar ao MongoDB
+client = MongoClient("mongodb://localhost:27017/")
+db = client["md_nutrition_db"]
+collection = db["data"]
+
+# Exportar para JSON
+cursor = collection.find()
+with open("saida.json", "w") as file:
+    for document in cursor:
+        file.write(json.dumps(document, default=str) + "\n")
